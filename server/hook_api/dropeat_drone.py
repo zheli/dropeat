b2baf7b64264fa58925397979c3f8e38304269d6
Narjis Hachilif <hnarjis@gmail.com>
2015-05-16 15:45:36 +0200

    Moved duo_port to a local file

--- server/hook_api/dropeat_drone.py
+++ server/hook_api/dropeat_drone.py
@@ -1,19 +1,22 @@
+import os
 import time
 from datetime import datetime
 from pyadrudino_serial import CommandManager
 
-duo_port = '/dev/cu.usbmodem141111'
 release_degree = "80"
 hook_degree = "180"
 
 
 class DroneHook(object):
     droped = False
-    cm = CommandManager(duo_port, 9600)
     status = None
     timestamp = None
 
     def __init__(self):
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(current_path, 'duo_port'), 'r') as content_file:
+            duo_port = content_file.read()
+        self.cm = CommandManager(duo_port, 9600)
         self.cm.send_cmd(hook_degree)
         self.droped = False
         self.timestamp = datetime.now()
